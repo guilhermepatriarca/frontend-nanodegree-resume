87bfedfdb8f300fa77b7c5930b2c3244142a461b
Guilherme Patriarca <guilhermejmpatriarca@gmail.com>
2016-06-06 14:41:53 +0100

    Meta --
    Object Literal

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,20 +12,59 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
 var skills = ["AngularJs","JavaScript"];
 //$("#main").append(skills);
 //$("#main").append(skills[0]);
 //$("#main").append(skills.length);
+
 var bio = {
   "name":"Guilherme",
   "role":"Web Developer",
   "contacts":{
     "mobile":"965547869",
     "email" :"guilhermejmpatriarca@gmail.com",
-    "location":"Coimbra"
+    "location":"Portugal"
   },
 "welcomeMessage":"Hello welcome",
 "skills": ["AngularJs","JavaScript","HTML"],
   "bioPic":"images/fry.jpg"
 };
-$("#main").append(bio.name);
+//$("#main").append(bio.name);
+
+var work ={};
+work.position="Estágio";
+work.employer="Sasuc";
+work.years="1";
+//$("#main").append(work["position"]);
+
+ var education = {
+   "schools":[
+     {
+   "location":"Portugal",
+   "city":"Coimbra",
+   "name":"Instituto Superior Miguel Torga",
+   "graduation":"Licenciatura",
+   "years":"2012-2015"
+ },{
+   "location":"Portugal",
+   "city":"Coimbra",
+   "name":"Arca",
+   "graduation":"Pos-Graudação",
+   "years":"2011-2012"
+ }
+ ],"onlineCoures":[
+   {
+     "title":"JavaScript Syntax",
+     "school" :"Udacity",
+     "dates":"2016",
+     "url":"http://www.idacity.com/course/ud804"
+   }
+ ]
+ }
+
+
+ // education["name"] ="Instituto Miguel Torga";
+ // education["years"]="2012-2015";
+ // education["city"]="Coimbra";
+//$("#main").append(education.name);
