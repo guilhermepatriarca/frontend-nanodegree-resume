6de5e15e21de2c3a6b6f480e2e462076eaa34091
Guilherme Patriarca <guilhermejmpatriarca@gmail.com>
2016-06-06 15:19:20 +0100

    JSON
    Work, bio, projects and education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,23 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var awseomeThoughts = "I am Guilherme and I am AWESOME!"
-console.log(awseomeThoughts);
-var funToughts = awseomeThoughts.replace("AWESOME", "FUN");
-console.log(funToughts);
-//$("#main").append(funToughts);
-var name = "Guilherme Patriarca";
-var role = "Web Developer";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var skills = ["AngularJs","JavaScript"];
+// var awseomeThoughts = "I am Guilherme and I am AWESOME!"
+// console.log(awseomeThoughts);
+// var funToughts = awseomeThoughts.replace("AWESOME", "FUN");
+// console.log(funToughts);
+// //$("#main").append(funToughts);
+// var name = "Guilherme Patriarca";
+// var role = "Web Developer";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+//
+// var skills = ["AngularJs","JavaScript"];
 //$("#main").append(skills);
 //$("#main").append(skills[0]);
 //$("#main").append(skills.length);
-
+//
 var bio = {
   "name":"Guilherme",
   "role":"Web Developer",
