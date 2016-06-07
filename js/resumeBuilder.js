ee302c0a6eb682a4a41a8b24b9925906edbe187f
Guilherme Patriarca <guilhermejmpatriarca@gmail.com>
2016-06-07 11:51:47 +0100

    Header
    Done  Top Contact's no yeat...

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,48 +1,69 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-// var awseomeThoughts = "I am Guilherme and I am AWESOME!"
-// console.log(awseomeThoughts);
-// var funToughts = awseomeThoughts.replace("AWESOME", "FUN");
-// console.log(funToughts);
-// //$("#main").append(funToughts);
-// var name = "Guilherme Patriarca";
-// var role = "Web Developer";
-// var formattedName = HTMLheaderName.replace("%data%", name);
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-//
-// var skills = ["AngularJs","JavaScript"];
-//$("#main").append(skills);
-//$("#main").append(skills[0]);
-//$("#main").append(skills.length);
-//
+// Json Bio
 var bio = {
-    "name": "Guilherme",
+    "name": "Guilherme Patriarca",
     "role": "Web Developer",
     "contacts": {
         "mobile": "965547869",
         "email": "guilhermejmpatriarca@gmail.com",
+        "github": "https://github.com/guilhermepatriarca",
+        "blog": "www.trynewblog.com",
         "location": "Portugal"
     },
-    "welcomeMessage": "Hello welcome",
-    "skills": ["AngularJs", "JavaScript", "HTML"],
+    "welcomeMessage": "Hello World!!",
+    "skills": ["Good Person", "Awesomeness", "Travel in future"],
     "bioPic": "images/fry.jpg"
 };
-//$("#main").append(bio.name);
-
-// var work ={};
-// work.position="Estágio";
-// work.employer="Sasuc";
-// work.years="1";
-//$("#main").append(work["position"]);
-
-// education["name"] ="Instituto Miguel Torga";
-// education["years"]="2012-2015";
-// education["city"]="Coimbra";
-//$("#main").append(education.name);
-// education JSON
+//**************** BIO
+// role
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+// name
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formattedName);
+// Contact Generic
+$("#header").append(HTMLcontactGeneric);
+// mobile
+var formattedmobile = HTMLmobile.replace("%data%", bio.contacts.mobile)
+$("#topContacts").append(formattedmobile);
+// email
+var formattedemail = HTMLemail.replace("%data%", bio.contacts.email)
+$("#topContacts").append(formattedemail);
+// github
+var github_change = bio.contacts.github
+var change_string = function(github_change) {
+    github_change = github_change.slice(19);
+    return github_change;
+}
+console.log(change_string(github_change))
+    // SLICE
+var formattedgithub = HTMLgithub.replace("%data%", change_string(github_change))
+$("#topContacts").append(formattedgithub);
+// blog
+var formattedblog = HTMLblog.replace("%data%", bio.contacts.blog)
+$("#topContacts").append(formattedblog);
+// location
+var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location)
+$("#topContacts").append(formattedlocation);
+// Pic
+var formattedboiPic = HTMLbioPic.replace("%data%", bio.bioPic)
+$("#header").append(formattedboiPic);
+// welcomeMessage
+var formattedwelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage)
+$("#header").append(formattedwelcomeMessage);
+// Skills
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+    $("#skills").append(formattedSkill);
+}
+// Json Education
 var education = {
         "schools": [{
             "location": "Portugal",
@@ -95,14 +116,4 @@ var projects = {
         "dates": "06/06/2016",
         "description": "Test 3"
     }]
-}
-console.log(bio.skills.length)
-if (bio.skills.length > 0) {
-    $("header").append(HTMLskillsStart);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
 }
